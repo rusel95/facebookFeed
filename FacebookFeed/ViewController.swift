357f0e0c1c64efab0c522f8ad06a2b778b6b3cf1
rusel95 <ruslanpopesku95@gmail.com>
2017-02-28 20:45:46 +0200

    Normal cell view with «picture», «like, share, comment» buttons

--- FacebookFeed/ViewController.swift
+++ FacebookFeed/ViewController.swift
@@ -34,7 +34,13 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 60)
+        return CGSize(width: view.frame.width, height: 400)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        collectionView?.collectionViewLayout.invalidateLayout()
     }
     
 }
@@ -56,14 +62,23 @@ class FeedCell : UICollectionViewCell {
         label.numberOfLines = 2
         
         let attributedText = NSMutableAttributedString(string: "Mark Zuckerberg", attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)])
-        attributedText.append(NSAttributedString(string: "\nDecember 18 * San Francisco * ", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 12), NSForegroundColorAttributeName: UIColor.init(red: 155/255, green: 161/255, blue: 171/255, alpha: 1) ]))
+        attributedText.append(NSAttributedString(string: "\nDecember 18 * San Francisco * ", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 12), NSForegroundColorAttributeName: UIColor.rgb(red: 155, green: 161, blue: 161) ]))
+    
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 4
         
-        attributedText.addAttribute(NSForegroundColorAttributeName, value: paragraphStyle, range: NSMakeRange(0, attributedText.string.characters.count))
+        attributedText.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, attributedText.string.characters.count))
         
         label.font = UIFont.boldSystemFont(ofSize: 14)
         label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(named: "globe_small")
+        attachment.bounds = CGRect(x: 0, y: -2, width: 12, height: 12)
+        attributedText.append(NSAttributedString(attachment: attachment))
+        
+        label.attributedText = attributedText
+        
         return label
     } ()
     
@@ -76,15 +91,91 @@ class FeedCell : UICollectionViewCell {
         return imageView
     } ()
     
+    let statusTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "MeanWhile, beast turned to the dark side."
+        textView.font = UIFont.systemFont(ofSize: 14)
+        return textView
+    } ()
+    
+    let statusImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "zuckdog")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true //that could help Ant to make pictures like in Instagramm
+        return imageView
+    } ()
+    
+    let likesCommentsLabel: UILabel = {
+            let label = UILabel()
+        label.text = "488 Likes   10.7K Comments"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor.rgb(red: 155, green: 161, blue: 171)
+        return label
+    } ()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(red: 226, green: 228, blue: 232)
+        return view
+    } ()
+    
+    let likeButton = FeedCell.buttonForTitle(title: "Like", imageName: "like")
+    let commentButton = FeedCell.buttonForTitle(title: "Comment", imageName: "comment")
+    let shareButton = FeedCell.buttonForTitle(title: "Share", imageName: "share")
+    
+    static func buttonForTitle(title: String, imageName: String) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(UIColor.rgb(red: 143, green: 150, blue: 263), for: .normal)
+        
+        button.setImage(UIImage(named: imageName) , for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 8)
+        
+        return button
+    }
+
     func setupViews() {
         backgroundColor = UIColor.white
         
         addSubview(nameLabel)
         addSubview(profileImageView)
+        addSubview(statusTextView)
+        addSubview(statusImageView)
+        addSubview(likesCommentsLabel)
+        addSubview(dividerLineView)
         
-        addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]-8-|", views: profileImageView, nameLabel)
-        addConstraintsWithFormat(format: "V:|-8-[v0(44)]|", views: profileImageView)
-        addConstraintsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        addSubview(likeButton)
+        addSubview(commentButton)
+        addSubview(shareButton)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
+        
+        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]-4-[v2]-8-[v3(24)]-8-[v4(0.4)][v5(44)]|", views: profileImageView, statusTextView, statusImageView, likesCommentsLabel, dividerLineView, likeButton)
+        
+        addConstraintsWithFormat(format: "V:|-12-[v0]", views: nameLabel)
+        
+        addConstraintsWithFormat(format: "H:|-4-[v0]-4-|", views: statusTextView)
+
+        addConstraintsWithFormat(format: "H:|-12-[v0]|", views: likesCommentsLabel)
+        
+        addConstraintsWithFormat(format: "H:|[v0]|", views: statusImageView)
+        
+        addConstraintsWithFormat(format: "H:|-12-[v0]-12-|", views: dividerLineView)
+        
+        //button constraints
+        addConstraintsWithFormat(format: "H:|[v0(v1)][v1(v2)][v2]|", views: likeButton, commentButton, shareButton)
+        
+        addConstraintsWithFormat(format: "V:[v0(44)]|", views: commentButton)
+        
+        addConstraintsWithFormat(format: "V:[v0(44)]|", views: shareButton)
+    }
+}
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
 }
 
